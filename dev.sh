a3adb5c922eba6fef0513ff7bbc67c1b67ead8aa
fabrizio2210 <fabrizio2210@gmail.com>
2019-11-05 10:00:07 +0100

    Now dev.sh is used to start server in VM

--- dev.sh
+++ dev.sh
@@ -10,4 +10,4 @@ vagrant up
 # Set infrastructure
 
 ansible-playbook -i vagrant.py -i vagrant-groups.list setWebserver-infrared.yml
-
+vagrant ssh -c "cd /opt/web-infrared/ ; source venv/bin/activate; python app.py"

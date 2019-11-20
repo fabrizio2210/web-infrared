5eae301ce0d871fc7422722cf687077e1dc279c9
fabrizio2210 <fabrizio2210@gmail.com>
2019-11-20 18:28:26 +0100

    First aproach to web interface

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
+from flask import render_template
 from resources.remote_control import RemoteControl
+from models import remote_control
 from resources.device import Device
 from resources.key import Key
+from resources import web_remote
 import socket
 
 # Get own IP
@@ -10,11 +13,14 @@ my_ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not 
 
 app = Flask(__name__)
 api = Api(app)
-
 api.add_resource(RemoteControl, '/remote_control')
 api.add_resource(Device, '/device/<string:name>')
 api.add_resource(Key, '/device/<string:device_name>/key/<string:key_name>')
 
+app.add_url_rule('/', view_func=web_remote.index)
+app.add_url_rule('/device/<string:name>/web', view_func=web_remote.get_remote_control_device)
+
 if __name__ == '__main__':
   app.run(host=my_ip, port=5000, debug=True)
+
 
---
+++ src/resources/web_remote.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from models.device import DeviceModel
+from models import remote_control
+
+def index():
+  return render_template('index.j2', title='Home', devices=remote_control.get_devices())
+
+def get_remote_control_device(name):
+  device = DeviceModel.find_by_name(name)
+  return render_template('remote_control.j2', title='Home', device=device.json())

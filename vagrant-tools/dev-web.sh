a245a20559e6d3519678b73eabf01db07b4dc9a3
fabrizio2210 <fabrizio2210@gmail.com>
2019-11-30 10:37:47 +0100

    Revisioned the infrastructure of vagrant environment

--- vagrant-tools/dev-web.sh
+++ vagrant-tools/dev-web.sh
@@ -2,12 +2,12 @@
 set -x
 set -e
 
-cd $(dirname $0)
+cd $(dirname $0)/dev-vm/
 
 # Vagrant up
 vagrant up
 
 # Set infrastructure
 
-ansible-playbook -i vagrant.py -i vagrant-groups.list ../setApp.yml
+ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../setApp.yml
 vagrant ssh -c "cd /opt/web-infrared/ ; source venv/bin/activate; python app.py"
--- vagrant-tools/test-setup.sh
+++ vagrant-tools/test-setup.sh
@@ -2,16 +2,18 @@
 set -x
 set -e
 
-cd $(dirname $0)
+cd $(dirname $0)/test-vm/
 
 # Vagrant up
 vagrant up
 
 # Set infrastructure
-
-ansible-playbook -i vagrant.py -i vagrant-groups.list ../setInfrared.yml
+ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../setInfrared.yml
 
 # Restart machine and rexport NFS sync folder if it is not present
 if echo $(vagrant ssh -c "[ ! -d /opt/web-infrared/conf ] && echo REBOOT") | grep REBOOT ; then
   vagrant reload
 fi
+
+# Deploy App
+ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../deployApp.yml

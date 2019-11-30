97f95707654eb4fd03cd646078e41cebcd7c14ea
fabrizio2210 <fabrizio2210@gmail.com>
2019-11-30 14:57:55 +0100

    Used a unique playbook to setup

--- vagrant-tools/test-setup.sh
+++ vagrant-tools/test-setup.sh
@@ -7,19 +7,8 @@ cd $(dirname $0)/test-vm/
 # Vagrant up
 vagrant up
 
-# Set infrastructure
-ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../setInfrared.yml
-
-# Restart machine and rexport NFS sync folder if it is not present
-if echo $(vagrant ssh -c "[ ! -d /opt/web-infrared/conf ] && echo REBOOT") | grep REBOOT ; then
-  vagrant reload
-fi
-
-# Deploy App
-ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../deployApp.yml
-
-# Setup App
-ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../setApp.yml
+# Setup all
+ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../setup.yml
 
 # Test
 vagrant ssh -c "cd /opt/web-infrared/ ; source venv/bin/activate; python tests/test-infra.py"

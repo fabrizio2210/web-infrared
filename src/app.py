49bcd7c075be35616cb5a43a259994ec72edc1f4
fabrizio2210 <fabrizio2210@gmail.com>
2020-04-12 12:45:41 +0200

    Test with unique pipeline

--- src/app.py
+++ src/app.py
@@ -22,4 +22,3 @@ if __name__ == '__main__':
 # Get own IP
   my_ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
   app.run(host=my_ip, port=5000, debug=True)
-

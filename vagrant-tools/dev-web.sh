0037c639256d45eb069e82d0a65a6e6396d56868
Fabrizio W <fabrizio2210@gamil.com>
2020-08-16 12:58:49 +0200

    Added tip

--- vagrant-tools/dev-web.sh
+++ vagrant-tools/dev-web.sh
@@ -9,5 +9,8 @@ vagrant up
 
 # Set infrastructure
 
+# for cleaning the venv
+# rm venv/*
+
 ansible-playbook -i ../vagrant.py -i ../vagrant-groups.list ../../ansible/lib/setApp.yml
 vagrant ssh -c "cd /opt/web-infrared/ ; source venv/bin/activate; python app.py"
